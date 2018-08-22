76e5d611e0b24529a806965e17004f025c9bc6cc
Danil Ivanov <nonamenix@gmail.com>
2018-08-22 22:27:06 +0300

    Add tests

--- aiohug/swagger/tests/test_handlers.py
+++ aiohug/swagger/tests/test_handlers.py
@@ -23,4 +23,26 @@ async def test_swagger_json(app, test_client):
     client = await test_client(app)
     resp = await client.get("/swagger.json")
     body = await resp.json()
-    assert body == {}
+    assert resp.status == 200
+    assert resp.content_type == 'application/json'
+
+    # assert body == {}
+
+
+async def test_swagger_yaml(app, test_client):
+    client = await test_client(app)
+    resp = await client.get("/swagger.yaml")
+    assert resp.status == 200
+    assert resp.content_type == 'text/yaml'
+    text = await resp.text()
+
+    # assert text == ''
+
+
+async def test_swagger(app, test_client):
+    client = await test_client(app)
+    resp = await client.get("/swagger/")
+    assert resp.status == 200
+    assert resp.content_type == 'text/html'
+
+    # text = await resp.text()
